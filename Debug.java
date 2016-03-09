7523b0842c16a46bcc81f36a3073fe93aeaa1961
Ty Petrochko <ty.petrochko@gmail.com>
2016-03-09 12:13:52 -0500

    Updated for testing java 7

--- Debug.java
+++ Debug.java
@@ -1,6 +1,6 @@
 public class Debug {
 
-    private static boolean DEBUG = false;
+    private static boolean DEBUG = true;
     public static void DEBUG(String s) {
 	if (DEBUG)
 	    System.out.println(s);
