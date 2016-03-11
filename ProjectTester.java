7d610783f3cfc48d51d9b58de0a4145d45104baa
Ty Petrochko <ty.petrochko@gmail.com>
2016-03-11 02:26:32 -0500

    Added upper parallelism threshold

--- ProjectTester.java
+++ ProjectTester.java
@@ -2,13 +2,13 @@ public class ProjectTester {
 
 	// usage: server, server name, port, files, time
 	public static void main (String [] args){
-		int [] parallelSchedule = {1, 3, 5, 10, 15, 20, 30, 40, 50, 75, 100, 150, 200, 300, 500, 700};
+		int [] parallelSchedule = {1, 3, 5, 10, 15, 20, 30, 40, 50, 75, 100, 150, 200, 300, 500, 700, 1000, 1500, 2000};
 
 		new SHTTPTestClient();
 
 		System.out.printf("%15s %15s %15s\n", "Threads", "MB/s", "Delay (ms)");
 
-		for (int i = 0; i < 16; i ++){
+		for (int i = 0; i < 19; i ++){
 			String [] argsx = {"-server", args[0], "-servname", args[1], "-port", args[2],
 				"-parallel", parallelSchedule[i] + "", "-files", 
 			args[3], "-T", args[4]};
