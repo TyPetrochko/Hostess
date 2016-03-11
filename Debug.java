0159045ba877bacd8087d66b26ba3aa0fa34c9a0
Ty Petrochko <ty.petrochko@gmail.com>
2016-03-11 00:23:53 -0500

    Debugging on server

--- Debug.java
+++ Debug.java
@@ -1,6 +1,6 @@
 public class Debug {
 
-    public static boolean DEBUG = false;
+    public static boolean DEBUG = true;
     public static void DEBUG(String s) {
 	if (DEBUG)
 	    System.out.println(s);
