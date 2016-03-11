9b97532d3eddeb3209d18844ddb56c10d83df9d0
Ty Petrochko <ty.petrochko@gmail.com>
2016-03-11 01:52:22 -0500

    Updated typoe

--- ProjectTester.java
+++ ProjectTester.java
@@ -6,7 +6,7 @@ public class ProjectTester {
 
 		new SHTTPTestClient();
 
-		System.out.printf("%15s %15s %15s\n", "Threads", "MB/s", "Mean Delay");
+		System.out.printf("%15s %15s %15s\n", "Threads", "MB/s", "Delay (ms)");
 
 		for (int i = 0; i < 16; i ++){
 			String [] argsx = {"-server", args[0], "-servname", args[1], "-port", args[2],
@@ -15,7 +15,7 @@ public class ProjectTester {
 
 			float [] metrics = SHTTPTestClient.configAndRun(argsx, false);
 
-			System.out.printf("%15d %15f %15f \n", parallelSchedule[i], metrics[0], metrics[1]);
+			System.out.printf("%15d %15f %15f \n", parallelSchedule[i], metrics[0], metrics[1] * 1000);
 			SHTTPTestClient.clearVars();
 		}
 	}
